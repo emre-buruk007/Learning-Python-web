8f5bf71a2519a2a0f644fc5c9a5d397fcae08e7c
emre-buruk007 <emre.buruk007@gmail.com>
2022-03-30 00:12:20 +0300

    Adding some code to get this code to work together with pandas and export the received results to both .csv and .xls file formats.

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pandas as pd
 
 link = input("Please input the link of a youtube video:\n")
 
+video_data = fetch_youtube_video(link)
+
+data_frame = pd.DataFrame.from_dict(video_data)
+data_frame.to_csv('result.csv', index=False, header=True)
+data_frame.to_excel('results.xls', index=False, header=True)
+print(data_frame)
