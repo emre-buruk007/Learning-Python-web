621cb146ad9b1b3397b82a1c9631368e3f1d3992
emre-buruk007 <emre.buruk007@gmail.com>
2022-04-02 13:24:54 +0300

    Swapped the columns and rows of the dataframe in order to better represent data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 video_data = fetch_youtube_video(input("Please input the link of a youtube video:\n"))
 
-data_frame = pd.DataFrame.from_dict(video_data, orient="index")
+data_frame = pd.DataFrame.from_dict(video_data, orient="index").T
 data_frame.to_csv('result.csv', index=True, header=True)
 data_frame.to_excel('results.xlsx', engine="xlsxwriter")
 print(data_frame)
