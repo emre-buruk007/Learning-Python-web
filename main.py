ca34a1822d70eda68290e0bdb34dec4d00371c52
emre-buruk007 <emre.buruk007@gmail.com>
2022-04-03 20:41:16 +0300

    Managed to remove the index, the "0" in front of the first row for better representation.

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import pandas as pd
 
 
 def export_to_csv(dataframe):
-    dataframe.to_csv('result.csv', index=True, header=True)
+    dataframe.to_csv('result.csv')
 
 
 def export_to_xlsx(dataframe):
     dataframe.to_excel('results.xlsx', engine="xlsxwriter")
 
 
-video_data = fetch_youtube_video(input("Please input the link of a youtube video:\n"))
+link = input("Please input the link of a youtube video:\n")
 
-data_frame = pd.DataFrame.from_dict(video_data, orient="index").T
+video_data = fetch_youtube_video(link)
+
+data_frame = pd.DataFrame.from_dict(video_data).set_index("VideoName")
 export_to_csv(data_frame)
 export_to_xlsx(data_frame)
