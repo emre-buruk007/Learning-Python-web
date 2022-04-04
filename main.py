0b6a60646aa66c8c1abce689136f9f39b05f4866
emre-buruk007 <emre.buruk007@gmail.com>
2022-04-04 14:06:09 +0300

    I fixed some bugs and made sure the code works with different videos.

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from fetch_youtube_video import fetch_youtube_video
 import pandas as pd
 
 
-def export_to_csv(dataframe):
-    dataframe.to_csv('result.csv')
+def export_to_csv(data):
+    data.to_csv('result.csv')
 
 
-def export_to_xlsx(dataframe):
-    dataframe.to_excel('results.xlsx', engine="xlsxwriter")
+def export_to_excel(data):
+    data.to_excel('results.xlsx', engine='xlsxwriter')
 
 
-link = input("Please input the link of a youtube video:\n")
+video_data = fetch_youtube_video(input("Please input the link of a youtube video:\n"))
 
-video_data = fetch_youtube_video(link)
+data_frame = pd.DataFrame.from_dict(video_data).set_index('VideoName')
 
-data_frame = pd.DataFrame.from_dict(video_data).set_index("VideoName")
 export_to_csv(data_frame)
-export_to_xlsx(data_frame)
+export_to_excel(data_frame)
